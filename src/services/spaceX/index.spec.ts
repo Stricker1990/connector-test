48cab7cabc6767b152619a38e0445d09fbe49d08
Kirill Pavlov <Stricker1990@gmail.com>
2024-05-27 00:45:37 +0400

    add validation test

--- src/services/spaceX/index.spec.ts
+++ src/services/spaceX/index.spec.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosError } from "axios";
 import SpaceXService, { SpaceXError, SpaceXResponse } from ".";
-import { error } from "console";
+import Rocket from "operations/rocketsOne/Rocket";
 
 jest.mock('axios');
 
@@ -14,6 +14,17 @@ const spaceXResponse: SpaceXResponse = {
 		"https://imgur.com/azYafd8.jpg"
 	]
 };
+
+const expectedRocket: Rocket = {
+    id: "5e9d0d95eda69955f709d1eb",
+    company: "SPACEX",
+    country: "Republic of the Marshall Islands",
+    cost_per_launch: {
+        amount: 6700000,
+        currency: 'USD'
+    },
+    main_image: "https://imgur.com/DaCfMsj.jpg"
+}
 
 describe("SpaceXService", () => {
     let spaceXService: SpaceXService | null = null;
@@ -29,7 +40,7 @@ describe("SpaceXService", () => {
             (axios.get as jest.Mock).mockResolvedValue(resp);
             const rocket = await spaceXService?.getRocketById("5e9d0d95eda69955f709d1eb");
             expect(axios.get).toHaveBeenCalledTimes(1);
-            expect(rocket).toEqual(rocket);
+            expect(rocket).toEqual(expectedRocket);
         });
         it("Shoud generate error for non 200 status", async () => {
             (axios.get as jest.Mock).mockRejectedValueOnce({
@@ -45,6 +56,19 @@ describe("SpaceXService", () => {
                 await spaceXService?.getRocketById("any_wrong_id");
             } catch(error) {
                 expect(error).toEqual(new SpaceXError("Not Found", 404));
+            }
+        });
+        it("Shoud generate error for invalid SpaceX response", async () => {
+            const wrongSpacexData: Partial<SpaceXResponse> = {...spaceXResponse};
+            delete wrongSpacexData.company;
+            const resp = {
+                status: 200, data: spaceXResponse
+            };
+            (axios.get as jest.Mock).mockResolvedValue(resp);
+            try {
+                await spaceXService?.getRocketById("5e9d0d95eda69955f709d1eb");
+            } catch(error) {
+                expect(error).toEqual(new SpaceXError("Wrong spaceX Service response", 500));
             }
         })
     });
